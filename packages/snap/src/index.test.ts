6e0d2dc6bb4e6ef5da264bdf83d7dec810803fff
Alex Kraiz <selfsurfer@gmail.com>
2024-01-19 20:32:26 +0400

    feat: fix linter errors

--- packages/snap/src/index.test.ts
+++ packages/snap/src/index.test.ts
@@ -1,17 +1,17 @@
-import { expect } from "@jest/globals";
-import { installSnap } from "@metamask/snaps-jest";
+import { expect } from '@jest/globals';
+import { installSnap } from '@metamask/snaps-jest';
 
-describe("onRpcRequest", () => {
-  it("throws an error if the requested method does not exist", async () => {
+describe('onRpcRequest', () => {
+  it('throws an error if the requested method does not exist', async () => {
     const { request, close } = await installSnap();
 
     const response = await request({
-      method: "foo",
+      method: 'foo',
     });
 
     expect(response).toRespondWithError({
       code: -32603,
-      message: "Method not found.",
+      message: 'Method not found.',
       stack: expect.any(String),
     });
 
