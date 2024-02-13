208e3bb6de688bce430cc536eb1b05c1546fc3e4
Alex Kraiz <selfsurfer@gmail.com>
2024-02-13 21:22:04 +0400

    feat: new functions of snap

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,2 +1,445 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {};
+import { rpcErrors } from '@metamask/rpc-errors';
+import type { OnCronjobHandler } from '@metamask/snaps-sdk';
+import {
+  ManageStateOperation,
+  heading,
+  panel,
+  text,
+} from '@metamask/snaps-sdk';
+import { isAddress, createPublicClient, http } from 'viem';
+import { mainnet, sepolia } from 'viem/chains';
+import { z } from 'zod';
+
+import ERC20 from './ERC20';
+import Tezoro from './Tezoro';
+import { sepoliaTokens, ethereumTokens } from './tokens-list';
+
+const stateSchema = z.object({
+  token: z.string().optional(),
+});
+
+export type State = z.infer<typeof stateSchema>;
+
+type SaveToken = {
+  method: 'saveToken';
+  params: {
+    token: string;
+  };
+};
+
+type DeleteToken = {
+  method: 'deleteToken';
+};
+
+type GetToken = {
+  method: 'getToken';
+};
+
+type CheckTokens = {
+  method: 'checkTokens';
+};
+
+type RequestAccounts = {
+  method: 'requestAccounts';
+};
+type GetAccounts = {
+  method: 'getAccounts';
+};
+
+type RequestType =
+  | SaveToken
+  | DeleteToken
+  | GetToken
+  | CheckTokens
+  | RequestAccounts
+  | GetAccounts;
+
+type OnRpcRequestHandler = (args: { request: RequestType }) => Promise<unknown>;
+
+type Network = 'mainnet' | 'sepolia';
+
+const network = 'mainnet' as Network;
+
+const API_URL =
+  network === 'mainnet'
+    ? 'https://tezoro.io/api'
+    : 'https://dev-987354.tezoro.io/api';
+
+const publicClient = createPublicClient({
+  chain: network === 'mainnet' ? mainnet : sepolia,
+  transport: http(),
+});
+
+const tokens = network === 'mainnet' ? ethereumTokens : sepoliaTokens;
+
+const THRESHOLD_MIN_USD = 2000;
+
+const evmAddress = z.string().refine(isAddress, (address) => ({
+  message: `${address} is not a valid address`,
+}));
+
+const backupSchema = z.object({
+  amount: z.string().optional(),
+  id: z.string(),
+  timestamp: z.number().int().nonnegative().catch(0),
+  contractAddress: evmAddress,
+  ownerAddress: evmAddress,
+  userHash: z.string(),
+  activeTransactionHash: z.string().optional(),
+  beneficiaries: z
+    .object({
+      address: z.string().optional(),
+      percent: z.number().optional(),
+    })
+    .array(),
+  dateTriggerTimestamp: z.number().int().nonnegative().catch(0),
+  restoreDateTimestamp: z.number().int().nonnegative(),
+  metaId: z.string(),
+  status: z.string().optional(),
+  inactiveMonthPeriod: z.number().optional(),
+  isLaunchByInactivePeriod: z.boolean().optional(),
+  isLaunchedByInactivePeriod: z.boolean().optional(),
+  executor: z.string().optional(),
+  nonce: z
+    .object({ value: z.number().optional(), date: z.number().optional() })
+    .optional(),
+});
+
+const getBackups = async (token: string) => {
+  const responseData = await fetch(`${API_URL}/user/backups`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const json: unknown = await responseData.json();
+  const parsedBackupsData = backupSchema.array().parse(json);
+  return parsedBackupsData;
+};
+
+const getPriceOfAssetQuotedInUSD = async (assetName: string) => {
+  if (
+    assetName === 'USDT' ||
+    assetName === 'USDC' ||
+    assetName === 'DAI' ||
+    assetName === 'USDP' ||
+    assetName === 'TUSD'
+  ) {
+    return 1;
+  }
+  if (assetName.startsWith('W')) {
+    assetName = assetName.slice(1); // remove W
+  }
+  try {
+    const response = await fetch(
+      `https://api.binance.com/api/v3/ticker/price?symbol=${assetName.toUpperCase()}USDT`,
+    );
+    const json = await response.json();
+    const parsedData = z
+      .object({
+        price: z.string(),
+      })
+      .parse(json);
+
+    return parseFloat(parsedData.price);
+  } catch {
+    return 0;
+  }
+};
+
+function assertIsWithMessage(
+  candidate: unknown,
+): asserts candidate is { message: unknown } {
+  if (typeof candidate !== 'object' || candidate === null) {
+    throw new Error('Assertion failed: candidate is not an object');
+  }
+  if (!('message' in candidate)) {
+    throw new Error(
+      'Assertion failed: candidate does not have a message property',
+    );
+  }
+}
+
+const checkTokens = async () => {
+  const state = await snap.request({
+    method: 'snap_manageState',
+    params: {
+      operation: ManageStateOperation.GetState,
+      encrypted: true,
+    },
+  });
+  if (state) {
+    const parsedState = stateSchema.safeParse(state);
+    if (parsedState.success) {
+      const { token } = parsedState.data;
+      if (token) {
+        try {
+          const backups = await getBackups(token);
+          const backupsTokenAddresses = await publicClient.multicall({
+            allowFailure: false,
+            contracts: backups.map(
+              (backup) =>
+                ({
+                  address: backup.contractAddress,
+                  abi: Tezoro,
+                  functionName: 'tokenAddress' as const,
+                } as const),
+            ),
+          });
+          const backupsState = await publicClient.multicall({
+            allowFailure: false,
+            contracts: backups.map(
+              (backup) =>
+                ({
+                  address: backup.contractAddress,
+                  abi: Tezoro,
+                  functionName: 'state' as const,
+                } as const),
+            ),
+          });
+
+          const activeBackups = backups
+            .map((backup, index) => {
+              const backupState = backupsState[index];
+              return {
+                ...backup,
+                tokenAddress: backupsTokenAddresses[index],
+                isTerminalState: backupState !== undefined && backupState > 3,
+              };
+            })
+            .filter((backup) => !backup.isTerminalState);
+
+          const rawAccounts = await ethereum.request({
+            method: 'eth_accounts',
+            params: [],
+          });
+          const accounts = z.array(z.string()).parse(rawAccounts);
+
+          let balancesByAccount: Partial<
+            Record<string, Partial<Record<string, string>>>
+          > = {};
+
+          for (const account of accounts) {
+            const balances = await publicClient.multicall({
+              allowFailure: false,
+              contracts: tokens.map(
+                (asset) =>
+                  ({
+                    address: asset.address,
+                    abi: ERC20,
+                    functionName: 'balanceOf',
+                    args: [account],
+                  } as const),
+              ),
+            });
+
+            const decimals = await publicClient.multicall({
+              allowFailure: false,
+              contracts: tokens.map((asset) => ({
+                address: asset.address,
+                abi: ERC20,
+                functionName: 'decimals',
+              })),
+            });
+
+            for (const [index, balance] of balances.entries()) {
+              const tokenData = tokens[index];
+              const decimal = decimals[index];
+              if (tokenData !== undefined && decimal !== undefined) {
+                const balanceInDecimal =
+                  parseFloat(balance.toString()) /
+                  10 ** parseInt(decimal.toString(), 10);
+                const priceOfAssetQuotedInUSD =
+                  await getPriceOfAssetQuotedInUSD(tokenData.label);
+                if (balanceInDecimal > 0) {
+                  const balanceInUSD =
+                    balanceInDecimal * priceOfAssetQuotedInUSD;
+                  const totalTokenBackupsAmount = activeBackups
+                    .filter(
+                      // filter backups by token address
+                      (backup) =>
+                        backup.tokenAddress?.toLowerCase() ===
+                        tokenData.address.toLowerCase(),
+                    )
+                    .reduce<number>((acc, backup) => {
+                      if (backup.amount !== undefined) {
+                        return acc + parseFloat(backup.amount);
+                      }
+                      return acc;
+                    }, 0);
+                  const notBackedUpAmount =
+                    balanceInUSD - totalTokenBackupsAmount;
+                  if (notBackedUpAmount > THRESHOLD_MIN_USD) {
+                    balancesByAccount = {
+                      ...balancesByAccount,
+                      [account]: {
+                        ...balancesByAccount[account],
+                        [tokenData.label]: balanceInUSD.toFixed(2),
+                      },
+                    };
+                  }
+                }
+              }
+            }
+          }
+
+          return {
+            isStatePresent: true,
+            isTokenPresent: true,
+            data: balancesByAccount,
+          };
+        } catch (error) {
+          assertIsWithMessage(error);
+          return {
+            isStatePresent: true,
+            isTokenPresent: true,
+            error: error.message,
+          };
+        }
+      }
+      return {
+        isStatePresent: true,
+        isTokenPresent: true,
+      };
+    }
+    return {
+      isStatePresent: true,
+      isTokenPresent: false,
+    };
+  }
+  return {
+    isStatePresent: false,
+    isTokenPresent: false,
+  };
+};
+
+export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
+  switch (request.method) {
+    case 'requestAccounts': {
+      const data = await ethereum.request({
+        method: 'eth_requestAccounts',
+        params: [],
+      });
+      return z.array(z.string()).parse(data);
+    }
+    case 'getAccounts': {
+      const data = await ethereum.request({
+        method: 'eth_accounts',
+        params: [],
+      });
+      return z.array(z.string()).parse(data);
+    }
+    case 'checkTokens': {
+      const data = await checkTokens();
+      return data;
+    }
+    case 'saveToken': {
+      const result = await snap.request({
+        method: 'snap_dialog',
+        params: {
+          type: 'confirmation',
+          content: panel([
+            heading('Would you like to connect your account?'),
+            text('The action binds your Tezoro account to the snap.'),
+          ]),
+        },
+      });
+      if (result === true) {
+        const { params } = request;
+
+        await snap.request({
+          method: 'snap_manageState',
+
+          params: {
+            operation: ManageStateOperation.UpdateState,
+            newState: {
+              token: params.token,
+            },
+            encrypted: true,
+          },
+        });
+        return true;
+      }
+      return false;
+    }
+
+    case 'getToken': {
+      const state = await snap.request({
+        method: 'snap_manageState',
+        params: {
+          operation: ManageStateOperation.GetState,
+          encrypted: true,
+        },
+      });
+
+      if (state === null) {
+        return null;
+      }
+      const parsedState = stateSchema.safeParse(state);
+      if (!parsedState.success) {
+        throw new Error(
+          `State is invalid: ${parsedState.error.errors.join(', ')}`,
+        );
+      }
+      return parsedState.data.token;
+    }
+
+    case 'deleteToken': {
+      await snap.request({
+        method: 'snap_manageState',
+
+        params: {
+          operation: ManageStateOperation.UpdateState,
+          newState: {},
+          encrypted: true,
+        },
+      });
+      return true;
+    }
+
+    default:
+      throw rpcErrors.methodNotFound({
+        data: {
+          // @ts-expect-error - We already handle all cases above.
+          method: request.method,
+        },
+      });
+  }
+};
+
+export const onCronjob: OnCronjobHandler = async ({ request }) => {
+  switch (request.method) {
+    case 'checkTokens':
+      {
+        const { data } = await checkTokens();
+        if (data) {
+          const tokensList = new Set<string>();
+          Object.values(data).map(async (balances) => {
+            if (balances) {
+              Object.keys(balances).forEach((token) => {
+                tokensList.add(token);
+              });
+            }
+          });
+          if (tokensList.size > 0) {
+            [...tokensList].map(async (token) => {
+              await snap.request({
+                method: 'snap_notify',
+                params: {
+                  type: 'native',
+                  message: `Protect ${token} from loss with on-chain backup & will`,
+                },
+              });
+            });
+          }
+        }
+      }
+      break;
+    default:
+      throw rpcErrors.methodNotFound({
+        data: {
+          method: request.method,
+        },
+      });
+  }
+};
---
+++ packages/snap/src/tokens-list.ts
@@ -0,0 +1,145 @@
+export const ethereumTokens = [
+  {
+    label: 'USDT',
+    address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+  },
+  {
+    label: 'USDC',
+    address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  },
+  {
+    label: 'WBTC',
+    address: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+  },
+  {
+    label: 'WETH',
+    address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+  },
+  {
+    label: 'WBNB',
+    address: '0xB8c77482e45F1F44dE1745F52C74426C631bDD52',
+  },
+  {
+    label: 'TON',
+    address: '0x582d872a1b094fc48f5de31d3b73f2d9be47def1',
+  },
+  {
+    label: 'DAI',
+    address: '0x6b175474e89094c44da98b954eedeac495271d0f',
+  },
+  {
+    label: 'TRX',
+    address: '0x50327c6c5a14dcade707abad2e27eb517df87ab5',
+  },
+  {
+    label: 'MATIC',
+    address: '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0',
+  },
+  {
+    label: 'SHIB',
+    address: '0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce',
+  },
+  {
+    label: 'LEO',
+    address: '0x2af5d2ad76741191d15dfe7bf6ac92d4bd912ca3',
+  },
+  {
+    label: 'INJ',
+    address: '0xe28b3b32b6c345a34ff64674606124dd5aceca30',
+  },
+  {
+    label: 'OKB',
+    address: '0x75231f58b43240c9718dd58b4967c5114342a86c',
+  },
+  {
+    label: 'ATOM',
+    address: '0x8D983cb9388EaC77af0474fA441C4815500Cb7BB',
+  },
+  {
+    label: 'LINK',
+    address: '0x514910771af9ca656af840dff83e8264ecf986ca',
+  },
+  {
+    label: 'TUSD',
+    address: '0x0000000000085d4780B73119b644AE5ecd22b376',
+  },
+  {
+    label: 'UNI',
+    address: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
+  },
+  {
+    label: 'IMX',
+    address: '0xf57e7e7c23978c3caec3c3548e3d615c346e79ff',
+  },
+  {
+    label: 'LDO',
+    address: '0x5a98fcbea516cf06857215779fd812ca3bef1b32',
+  },
+  {
+    label: 'FDUSD',
+    address: '0xc5f0f7b66764F6ec8C8Dff7BA683102295E16409',
+  },
+  {
+    label: 'CRO',
+    address: '0xa0b73e1ff0b80914ab6fe0444e65848c4c34450b',
+  },
+  {
+    label: 'MNT',
+    address: '0x3c3a81e81dc49a522a592e7622a7e711c06bf354',
+  },
+  {
+    label: 'MKR',
+    address: '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2',
+  },
+  {
+    label: 'RNDR',
+    address: '0x6de037ef9ad2725eb40118bb1702ebb27e4aeb24',
+  },
+  {
+    label: 'ARB',
+    address: '0xB50721BCf8d664c30412Cfbc6cf7a15145234ad1',
+  },
+  {
+    label: 'QNT',
+    address: '0x4a220e6096b25eadb88358cb44068a3248254675',
+  },
+  {
+    label: 'GRT',
+    address: '0xc944e90c64b2c07662a292be6244bdf05cda44a7',
+  },
+  {
+    label: 'AAVE',
+    address: '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9',
+  },
+  {
+    label: 'YFI',
+    address: '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e',
+  },
+  {
+    label: 'FTM',
+    address: '0x4e15361fd6b4bb609fa63c81a2be19d873717870',
+  },
+  {
+    label: 'ANT',
+    address: '0xa117000000f279d81a1d3cc75430faa017fa5a2e',
+  },
+  {
+    label: 'DEC',
+    address: '0x30f271c9e86d2b7d00a6376cd96a1cfbd5f0b9b3',
+  },
+] as const;
+
+export const sepoliaTokens = [
+  {
+    label: 'USDT',
+    address: '0x02c53A005E1C0abc23967Ef440cB6615049d1444',
+  },
+  {
+    label: 'USDC',
+    address: '0x6f14C02Fc1F78322cFd7d707aB90f18baD3B54f5',
+  },
+  {
+    label: 'WBTC',
+    address: '0x536BcBE548cef2cE493932fEFCeC059Dda4d5579',
+  },
+] as const;
